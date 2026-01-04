804246f7baad0888f86f8628596291f2dcfab776
Manik Maity <manikmaity156@gmail.com>
2026-01-04 23:16:23 +0530

    Time: 3 ms (7.50%) | Memory: 54.3 MB (30.00%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
@@ -3,27 +3,52 @@
  * @return {boolean}
  */
 var check = function(nums) {
-    function checkSorted(arr = []) {
-      let isSorted = true;
-        for(let i = 0; i < arr.length - 1; i++) {
-          if (arr[i] > arr[i + 1]){
-            isSorted = false;
-          }
+
+    // BRUTE FORSE WAY
+    // function checkSorted(arr = []) {
+    //   let isSorted = true;
+    //     for(let i = 0; i < arr.length - 1; i++) {
+    //       if (arr[i] > arr[i + 1]){
+    //         isSorted = false;
+    //       }
+    //     }
+    //     return isSorted;
+    // }
+
+    // let n = nums.length;
+    // for (let i = 0; i < n; i++){
+    //   const is = checkSorted(nums);
+    //   if (is) {
+    //     return true;
+    //   }
+    //   let ele =  nums.shift();
+    //   nums.push(ele);
+    //   console.log(nums)
+    // }
+
+    // return false;
+
+
+    // MORE OPTIMIZED WAY - TWO POINTERS
+    const arr = [...nums, ...nums];
+    const n = arr.length;
+    
+    let r = 0;
+    let count = 1;
+    while(r < arr.length - 1) {
+        if (arr[r + 1] >= arr[r]) {
+            r++;;
+            count++;
         }
-        return isSorted;
-    }
+        else {
+            r++;
+            count = 1
+        }
 
-    let n = nums.length;
-    for (let i = 0; i < n; i++){
-      const is = checkSorted(nums);
-      if (is) {
-        return true;
-      }
-      let ele =  nums.shift();
-      nums.push(ele);
-      console.log(nums)
-    }
+        if (count >= nums.length){
+            return true
+        }
+    };
 
-    return false;
-  
+    return false
 };
